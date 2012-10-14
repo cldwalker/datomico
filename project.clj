f18d927dc9190fc22a6a4e7cbcc959f1785acc42
Gabriel Horner <gabriel.horner@gmail.com>
2012-10-14 19:44:24 -0400

    add datomic-free for testing/repl

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/cldwalker/datomic-simple"
   :license {:name "The MIT License"
             :url "https://en.wikipedia.org/wiki/MIT_License"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.8.3551"]]}})
