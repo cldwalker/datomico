fea0cfb227b07f9980ae2b0532613f889b5dcfea
Gabriel Horner <gabriel.horner@gmail.com>
2013-02-09 10:22:38 -0500

    add more useful test-selectors

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.8.3789"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.5"]})
+  :aliases {"all" ["with-profile" "dev:dev,1.5"]}
+  :test-selectors {:focus :focus :default (constantly true)})
