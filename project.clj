5979f0754f22be408d57fd0403837bc883e0cc9e
Gabriel Horner <gabriel.horner@gmail.com>
2013-06-01 22:48:57 -0400

    test against a legit 1.5

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "https://en.wikipedia.org/wiki/MIT_License"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.8.3789"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.5"]}
   :test-selectors {:focus :focus :default (constantly true)})
